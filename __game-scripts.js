f0ff013d212efda8fe9308e8662e71ea2ef68558
appaso <appaso1686@gmail.com>
2023-01-20 11:40:07 +0530

    Update __game-scripts.js
    xsasdd

--- __game-scripts.js
+++ __game-scripts.js
@@ -1,4 +1,1 @@
-var ButtonLogic=pc.createScript("buttonLogic");ButtonLogic.attributes.add("textEntity",{type:"entity",description:"The entity that we want to update when the button is clicked"}),ButtonLogic.attributes.add("description",{type:"string"});let i=0;ButtonLogic.prototype.initialize=function(){this.entity.button.on("click",(function(t){this.textEntity.element.text=this.description+" "+i++
- console.log("Testing button click");
-}
-),this)};
+var ButtonLogic=pc.createScript("buttonLogic");ButtonLogic.attributes.add("textEntity",{type:"entity",description:"The entity that we want to update when the button is clicked"}),ButtonLogic.attributes.add("description",{type:"string"});let i=0;ButtonLogic.prototype.initialize=function(){this.entity.button.on("click",(function(t){this.textEntity.element.text=this.description+" "+i++,console.log("Testing button click")}),this)};
